1d6a36b39c0d3caa8c1777712021dde74694a06a
Tomas Sys <tomas.sys96@gmail.com>
2023-12-12 12:14:26 +0100

    Detect numbers on lines

--- day_03/main.py
+++ day_03/main.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
+
 def read_file(filename: str) -> list[str]:
-    """Reads a text file in the current directory and returns it as a list of strings.
+    """Reads a text file in the current directory and returns its contents as a list of strings.
 
     Args:
         filename: Name of the text file to be read
@@ -9,13 +12,28 @@ def read_file(filename: str) -> list[str]:
     """
 
     with open(file=filename, mode="r") as file:
-        return file.readlines()
+        return [line.replace("\n", "") for line in file.readlines()]
 
 
 def main() -> None:
     """"""
 
     field: list[str] = read_file(filename="puzzle_input.txt")
+
+    digits: list[str] = []
+    number: Optional[int] = None
+
+    for line in field:
+        for character_index, character in enumerate(line):
+            if character.isdigit():
+                digits.append(character)
+            elif not line[character_index + 1].isdigit() and digits:
+                number = int("".join(digits))
+                digits = []
+
+            if number:
+                print("some logic")
+                number = None
 
 
 if __name__ == "__main__":
