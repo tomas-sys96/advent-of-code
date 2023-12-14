069d860870bdf4d991d20c09689c34f7184e5568
Tomas Sys <tomas.sys96@gmail.com>
2023-12-14 19:53:46 +0100

    Add Number and Symbol dataclasses to common.py

--- day_03/common.py
+++ day_03/common.py
@@ -1,3 +1,32 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Number:
+    start_index: int
+    stop_index: int
+    line_index: int
+    line: str
+    previous_line: Optional[str] = None
+    next_line: Optional[str] = None
+
+
+@dataclass
+class Symbol:
+    index: int
+    line_index: int
+    line: str
+    previous_line: Optional[str] = None
+    next_line: Optional[str] = None
+
+    def update_lines(self, previous_line: Optional[str], next_line: Optional[str]) -> None:
+        """"""
+
+        self.previous_line = previous_line
+        self.next_line = next_line
+
+
 def read_file(file_path: str) -> list[str]:
     """Reads a text file and returns its contents as a list of strings.
 
