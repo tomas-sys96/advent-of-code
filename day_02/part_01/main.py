da25c425f0d173c678ad68d9ec9c6fd10adbd8ef
Tomas Sys <tomas.sys96@gmail.com>
2023-12-16 12:09:00 +0100

    Create day_02, part_01

---
+++ day_02/part_01/main.py
@@ -0,0 +1,13 @@
+from day_02.helpers.common import read_file
+
+FILE_PATH: str = "../puzzle_input.txt"
+
+
+def main() -> None:
+    """"""
+
+    lines: list[str] = read_file(file_path=FILE_PATH)
+
+
+if __name__ == "__main__":
+    main()
