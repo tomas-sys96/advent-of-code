f5c288cc1a5fc06797195673ea598b4c2d55693d
Tomas Sys <tomas.sys96@gmail.com>
2023-12-16 11:10:34 +0100

    Add get_first_digit()

--- day_01/part_01/main.py
+++ day_01/part_01/main.py
@@ -3,28 +3,34 @@ from day_01.helpers.common import read_file
 FILE_PATH: str = "../puzzle_input.txt"
 
 
+def get_first_digit(array: str | reversed) -> str:
+    """Returns the first digit in an array.
+
+    Args:
+        array: Iterable string object
+
+    Returns:
+        Digit as a string
+    """
+
+    for character in array:
+        if character.isdigit():
+            return character
+
+
 def main() -> None:
     """Prints the sum of all the calibration values."""
 
     lines: list[str] = read_file(file_path=FILE_PATH)
-    first_digit: str = ""
-    last_digit: str = ""
     calibration_values_sum: int = 0
 
     for line in lines:
-        for character in line:
-            if character.isdigit():
-                first_digit = character
-                break
-        for character in reversed(line):
-            if character.isdigit():
-                last_digit = character
-                break
+        # For the first digit, iterate over the line in the forward direction
+        first_digit: str = get_first_digit(array=line)
+        # For the last digit, iterate over the line in the backward direction
+        last_digit: str = get_first_digit(array=reversed(line))
 
-        if first_digit and last_digit:
-            calibration_values_sum += int("".join([first_digit, last_digit]))
-            first_digit = ""
-            last_digit = ""
+        calibration_values_sum += int(first_digit + last_digit)
 
     print(calibration_values_sum)
 
