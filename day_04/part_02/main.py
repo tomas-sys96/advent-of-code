e3049b16f8db2881ef356adac537d3b7f72e028f
Tomas Sys <tomas.sys96@gmail.com>
2023-12-17 14:38:22 +0100

    Add docstrings

--- day_04/part_02/main.py
+++ day_04/part_02/main.py
@@ -4,7 +4,14 @@ FILE_PATH: str = "../puzzle_input.txt"
 
 
 class Card:
-    """"""
+    """Definition of a card.
+
+    Attributes:
+        number: Card number
+        winning_numbers: Winning numbers
+        owned_numbers: Numbers that a person owns
+        points: Total points for the card
+    """
 
     def __init__(self, number: int, winning_numbers: list[int], owned_numbers: list[int]) -> None:
         self.number: int = number
@@ -13,14 +20,18 @@ class Card:
         self.points: int = 0
 
     def get_points_for_card(self) -> None:
-        """"""
+        """Calculates the sum of points for the card."""
 
         for number in self.owned_numbers:
             if number in self.winning_numbers:
                 self.points += 1
 
     def get_next_card_numbers(self) -> list[int]:
-        """"""
+        """Returns a list of the next card numbers based on the current card number and points.
+
+        Returns:
+            List of the next card numbers
+        """
 
         self.get_points_for_card()
         return list(range(self.number + 1, self.number + self.points + 1))
@@ -50,11 +61,14 @@ def main() -> None:
             )
 
             next_card_numbers: list[int] = card.get_next_card_numbers()
-            for number in next_card_numbers:
+            for card_number in next_card_numbers:
                 try:
-                    card_instances[f"card_{number}"] += card_instances[f"card_{current_card_number}"]
+                    card_instances[f"card_{card_number}"] += card_instances[f"card_{current_card_number}"]
                 except KeyError:
-                    card_instances[f"card_{number}"] = card_instances[f"card_{current_card_number}"]
+                    card_instances[f"card_{card_number}"] = card_instances[f"card_{current_card_number}"]
+
+            # Delete the card object from memory
+            del card
 
     print(sum(card_instances.values()))
 
