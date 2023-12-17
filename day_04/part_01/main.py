de9e84af5887a635a9b992b559aefee67aa94d33
Tomas Sys <tomas.sys96@gmail.com>
2023-12-17 11:36:44 +0100

    Finish first part of Day 4

--- day_04/part_01/main.py
+++ day_04/part_01/main.py
@@ -2,7 +2,16 @@ FILE_PATH: str = "../puzzle_input.txt"
 
 
 def separate_numbers(numbers: str) -> tuple[list[int], list[int]]:
-    """"""
+    """Separates the winning and owned numbers.
+
+    Args:
+        numbers: All card numbers
+
+    Returns:
+        tuple:
+            winning_numbers: Winning numbers,
+            owned_numbers: Numbers that a person owns
+    """
 
     winning_numbers: list[int] = [int(number.strip()) for number in numbers.split("|")[0].split()]
     owned_numbers: list[int] = [int(number.strip()) for number in numbers.split("|")[1].split()]
@@ -11,7 +20,15 @@ def separate_numbers(numbers: str) -> tuple[list[int], list[int]]:
 
 
 def get_points_from_card(winning_numbers: list[int], owned_numbers: list[int]) -> int:
-    """"""
+    """Calculates the sum of points per one card.
+
+    Args:
+        winning_numbers: Winning numbers
+        owned_numbers: Numbers that a person owns
+
+    Returns:
+        Points per one card
+    """
 
     points: int = 0
     for number in owned_numbers:
@@ -25,9 +42,9 @@ def get_points_from_card(winning_numbers: list[int], owned_numbers: list[int]) -
 
 
 def main() -> None:
-    """"""
+    """Prints the points sum of a pile of scratchcards."""
 
-    scratchcard_points: int = 0
+    scratchcard_points_sum: int = 0
 
     with open(file=FILE_PATH, mode="r") as file:
         while True:
@@ -39,9 +56,9 @@ def main() -> None:
             owned_numbers: list[int]
             winning_numbers, owned_numbers = separate_numbers(numbers=line.split(":")[1])
 
-            scratchcard_points += get_points_from_card(winning_numbers=winning_numbers, owned_numbers=owned_numbers)
+            scratchcard_points_sum += get_points_from_card(winning_numbers=winning_numbers, owned_numbers=owned_numbers)
 
-    print(scratchcard_points)
+    print(scratchcard_points_sum)
 
 
 if __name__ == "__main__":
