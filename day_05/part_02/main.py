1d33d890e674ce3f3cf7c6b062c600aaba76d135
Tomas Sys <tomas.sys96@gmail.com>
2023-12-22 12:47:17 +0100

    Resolve selected bugs in get_location_for_seed_range(), add TODO

--- day_05/part_02/main.py
+++ day_05/part_02/main.py
@@ -23,6 +23,15 @@ def get_seed_ranges(line: str) -> list[range]:
 def get_location_for_seed_range(seed_range: range, maps: list[list[ConversionMap]]) -> int:
     """"""
 
+    # TODO: Create the Range dataclass, and make destination_ranges a set of these ranges
+    # class Range:
+    #     def __init__(self, start, end):
+    #         self.start = start
+    #         self.end = end
+    #
+    #     def get_range_object(self):
+    #         return range(self.start, self.end)
+
     destination_ranges: list[range] = [seed_range]
     source_ranges: list[range]
 
@@ -30,18 +39,25 @@ def get_location_for_seed_range(seed_range: range, maps: list[list[ConversionMap
         source_ranges = destination_ranges.copy()
         destination_ranges.clear()
         for source_range in source_ranges:
-            source_range_start: int = source_range.start
-            range_length: int = source_range.stop - source_range.start
             for conversion_map in mapping:
+                source_range_start: int = source_range.start
+                range_length: int = source_range.stop - source_range.start
                 conversion_map_range: range = range(
                     conversion_map.source_range_start,
                     conversion_map.source_range_start + conversion_map.range_length,
                 )
 
                 # Check if there's a full/partial overlap of ranges
-                if any(source in conversion_map_range for source in [source_range.start, source_range.stop]):
-                    offset: int = conversion_map.destination_range_start - conversion_map.source_range_start
+                if any(source in conversion_map_range for source in [source_range.start, source_range.stop - 1]):
+                    # In case of an overlap, delete the original source range from the list of destination ranges;
+                    # it'll be replaced by the shifted range (full overlap), or by the shifted and non-shifted parts
+                    # of the range (partial overlap)
+                    # TODO: Uncomment if necessary
+                    # if original_range in destination_ranges:
+                    #     destination_ranges.remove(original_range)
 
+                    # Check if the first source falls into the conversion map range
+                    # If it doesn't, separate a new range from the original source range
                     if source_range.start not in conversion_map_range:
                         destination_ranges.append(
                             range(
@@ -49,25 +65,40 @@ def get_location_for_seed_range(seed_range: range, maps: list[list[ConversionMap
                                 conversion_map_range.start,
                             )
                         )
+                        # Set new range start and length accordingly
                         source_range_start = conversion_map_range.start
+                        range_length = source_range.stop - source_range_start
 
+                    # Check if the last source falls into the conversion map range
+                    # If it doesn't, separate a new range from the original source range
                     if source_range.stop - 1 not in conversion_map_range:
                         destination_ranges.append(
                             range(
-                                conversion_map_range.stop + 1,
+                                conversion_map_range.stop,
                                 source_range.stop,
                             )
                         )
-                        range_length = conversion_map_range.stop - source_range_start
+                        range_length = conversion_map_range.stop - source_range.start
 
-                    source_range_start += offset
+                    shift: int = conversion_map.destination_range_start - conversion_map.source_range_start
 
-            destination_ranges.append(
-                range(
-                    source_range_start,
-                    source_range_start + range_length,
+                    # Add the overlapping subset of the original source range to the list of destination ranges
+                    destination_ranges.append(
+                        range(
+                            source_range_start + shift,
+                            source_range_start + shift + range_length,
+                        )
+                    )
+
+            if not destination_ranges:
+                # Add the whole unchanged source range to the list of destination ranges
+                # if there are no overlaps at all
+                destination_ranges.append(
+                    range(
+                        source_range.start,
+                        source_range.stop,
+                    )
                 )
-            )
 
 
 def main() -> None:
